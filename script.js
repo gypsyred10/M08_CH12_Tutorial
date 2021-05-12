9226e524bc39c9791cd06cb454436779ea0b17b9
gypsyred10 <82721123+gypsyred10@users.noreply.github.com>
2021-05-12 14:21:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*  JavaScript 6th Edition
+    Chapter 12
+	Chapter case
+	Life on Rocks Wildlife Cruises
+	Author: Asher Loyd
+	Date: 5/12/21
+	Filename: script.js
+*/
+$("ul.mainmenu li").children("ul").addClass("show");
+$("ul.mainmenu li").children("ul").addClass("show");
+
+function display $(event.currentTarget).children("ul").addClass("show");) {
+	
+}
+function hide$("ul.mainmenu li").hover(display,hide);) {   $(event.currentTarget).children("ul").removeClass("show");
+}
